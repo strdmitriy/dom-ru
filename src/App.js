b4077c7d3fa2e8778feb59a637722329e6331b4a
Dmitriy Strungar <dmitriy.st92@mail.ru>
2018-03-04 20:49:37 +0500

    Creat react-app and add block for promo

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Promo from './components/promo/promo';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <Promo/>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/promo/promo.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import logo from '../img/Logotype.png';
+
+class Promo extends Component {
+  render() {
+    return (
+      <section className="promo">
+        <div className="promo__container">
+          <div className="logo">
+            <img src={logo} className="promo__logo" alt="logo" />
+          </div>
+          <h1 className="promo__title">Где лучшее кино? Друзья подскажут!</h1>
+          <p className="promo__text">
+          Наши Клиенты точно знают, где можно раздобыть самые свежие, впечатляющие,
+          удивительные и суперинтересные фильмы.
+          Конечно, в пакете каналов Viasat Premium HD!
+          </p>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default Promo;
